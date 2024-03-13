c762ed5f1ca7c99db9632b4903f7bed61a0ead17
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-03-13 16:21:57 +0500

    faqat details page qoldi

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ import Restuarant from './pages/app/orderLandingPage/restuarants'
 import Cafe from './pages/app/orderLandingPage/cafe'
 import LocationMap from './pages/app/orderLandingPage/location-map'
 import DeparutreTame from './pages/app/orderLandingPage/departure-tame'
+import ComplateTaxiBooking from './pages/app/orderLandingPage/complate-taxi-booking'
+import Details from './pages/app/orderLandingPage/components/details'
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
       <Route path='/order-landing page/restuarant' Component={Restuarant} />
       <Route path='/order-landing page/cafe' Component={Cafe} />
       <Route path='/departure-tame' Component={DeparutreTame} />
+      <Route path='/complate taxi-booking' Component={ComplateTaxiBooking} />
+      <Route path='/details' Component={Details} />
       <Route path='/map-page' Component={LocationMap} />
     </Routes>
   )
---
+++ src/pages/app/orderLandingPage/complate-taxi-booking.jsx
@@ -0,0 +1,42 @@
+import BookingMenu from "./components/booking-menu";
+import FirstSection from "./components/firstSection";
+import './styles.css';
+import AboutFinder from "./components/about-finder";
+import Footer from "./components/footer";
+import ReservationRejection from "./components/reservation-rejection";
+
+const ComplateTaxiBooking = () => {
+    return (
+        <div className="w-full text-black landing-main">
+            <div className="max-w-[1350px] mx-auto">
+                <div className="w-full h-screen">
+                    <FirstSection
+                        heading={`Online Hospitality facility finder system a right place.`}
+                        text='In case you want to access the near by hospitality facilities services this is the  right place for you.'
+                        headingTwo='want nearby facilities on the map? click below'
+                    />
+                </div>
+                <BookingMenu
+                    className='mr-0'
+                    leftText='Book a car'
+                    rightText='Back to home page'
+                    button=''
+                    leftLink='/'
+                    rightLink='/'
+                />
+            </div>
+            <div className="w-[98%] mx-auto mt-6 border-2 border-gray-300 rounded-md">
+                <ReservationRejection />
+            </div>
+            <div className="max-w-[1350px] mx-auto mt-20 pb-24 border-b-2 border-gray-400">
+                <AboutFinder
+                    title='About HFfinder'
+                    text='This is the system that links the tourists and hospitality facilities owners. this is done in such way that people can view the nearby their location Hospitality facilitites and be able to access their services aswell as pay on the system. '
+                />
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default ComplateTaxiBooking;
